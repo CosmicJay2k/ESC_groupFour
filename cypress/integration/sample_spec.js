a4f641f44fac2ab2eba0f6ac073af6d8ebcb4296
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-02-16 10:29:03 +0100

    Refactor cypress test code

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,18 @@
-describe("Learning Cypress", () => {
+describe("Using Cypress", () => {
   it("to visit ESC on localhost", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
+  });
+
+  it("to find a link and click it", () => {
+    cy.contains("Play online").click();
+  });
+
+  it("to verify that the link was clicked", () => {
+    cy.contains("Filter challenges");
+  });
+
+  it("to visit ESC on GitHub Pages", () => {
+    cy.visit("https://cosmicjay2k.github.io/ESC_groupFour/");
   });
 
   it("to find a link and click it", () => {
